6cc12184b44545d6f3ca5220f1c192fbf155df6a
Haseeb Ahmad Tahir <hatahir87626@gmail.com>
2024-08-01 04:10:32 +0500

    m

---
+++ travle_slicer/src/features/travel_suggestion_slice.jsx
@@ -0,0 +1,15 @@
+import { createSlice, nanoid } from '@reduxjs/toolkit';
+
+// const initialState ={
+//     suggestion : [
+//         {text: 'Sorry!, The key word you entered is not in our data.'}
+//     ]
+// }
+
+// export const suggestionSlice = createSlice({
+//     name: 'suggestion',
+//     initialState,
+//     reducers: {
+
+//     } 
+// })
